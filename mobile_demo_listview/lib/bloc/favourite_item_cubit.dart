0f2f5cd8fd9a194750b139c651dc734076003271
JiwonHae <hjiwon1994@gmail.com>
2022-06-30 02:23:21 +0900

    fix(bloc): fix cubit state type

--- mobile_demo_listview/lib/bloc/favourite_item_cubit.dart
+++ mobile_demo_listview/lib/bloc/favourite_item_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:mobile_demo_listview/bloc/favourite_item_state.dart';
+import 'package:mobile_demo_listview/domain/entities/favourite.dart';
 
-class FavouriteItemCubit extends Cubit<FavouriteItemState>{
-  FavouriteItemCubit(super.state);
+import '../domain/usecases/favourite_usescase.dart';
 
+class FavouriteItemCubit extends Cubit<List<Favourite>>{
+  FavouriteItemCubit({required this.usecase}) : super(List.empty());
+
+  final FavoriteItemUseCase usecase;
+
+  void addItem(Favourite item){
+    usecase.add(item);
+    emit(usecase.items);
+  }
+
+  void removeItem(Favourite item){
+    usecase.remove(item);
+    emit(usecase.items);
+  }
 
 }
